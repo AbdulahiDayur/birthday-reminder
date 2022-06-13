0f683c4176d1a6e031093dcf07b125ee058f8cb8
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-06-12 21:49:37 -0700

    update styling and architecture

--- src/List.jsx
+++ src/List.jsx
@@ -3,13 +3,16 @@ import React from "react";
 function List({ people }) {
   return (
     <>
-      {people.map((obj, index) => {
+      {people.map((person) => {
+        const { id, name, age, image } = person;
         return (
-          <div className="person">
-            <img src={obj.image} alt=""></img>
-            <h4>{obj.name}</h4>
-            <p>{obj.age} years old</p>
-          </div>
+          <article key={id} className="person">
+            <img src={image} alt=""></img>
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years old</p>
+            </div>
+          </article>
         );
       })}
     </>
